1000750052c4e4123bf3d0cd0b1b01586739d251
Lonna Barlow <lonna@lemonblock.com>
2022-01-21 13:03:59 -0700

    fix location of password.js

---
+++ password.js
@@ -0,0 +1,18 @@
+
+const readline = require("readline")
+console, log(" Welcome to the  password validator tool")
+
+
+const reader = createPassword({
+input: process.stdin,
+output: process.stdout,
+});
+
+const question = ["Enter Password"]
+for (let i = 0; question.length; i++){
+if(question[i] === 9){
+    console.log("Welcome friend")
+}else{
+    console.log("Whoops, try again")
+}
+}
